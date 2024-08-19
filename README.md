8df3696ed1cc6647f71616561236c483328dc1fa
Rajib Kumar das <106542671+RajibKumar7@users.noreply.github.com>
2024-08-19 07:06:38 +0530

    Files Uploaded ✅

---
+++ README.md
@@ -0,0 +1,22 @@
+![aifile store](https://telegra.ph/Ai-png-12-11)
+
+⍟ <b>𝗧𝗵𝗶𝘀 𝗶𝘀 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗣𝗮𝗿𝗺𝗮𝗻𝗲𝗻𝘁 𝗙𝗶𝗹𝗲𝘀 𝗦𝘁𝗼𝗿𝗲 𝗕𝗼𝘁 𝗯𝘆 [AI BOTZ](https://t.me/Ai_botz_owner_bot).<b>
+
+* **Language:** [Python3](https://www.python.org)
+* **Library:** [Pyrogram](https://docs.pyrogram.org)
+* 
+### Deploy On Heroku 
+<details><summary>Deploy To Heroku</summary>
+<br>
+<a href="https://heroku.com/deploy?template=https://github.com/tgaibotz/filestore-urlsh">
+<img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy to Heroku">
+</a>
+</details>
+
+### Demo Bot:
+<a href="https://telegram.me/ai_file_store_robot"><img src="https://img.shields.io/badge/Demo-Telegram%20Bot-blue.svg?logo=telegram"></a>
+
+### 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩:
+<a href="https://t.me/AI_BOTZ_SUPPORT"><img src="https://img.shields.io/badge/Telegram-Join%20Telegram%20Group-green.svg?logo=telegram"></a>
+### 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥:
+<a href="https://telegram.me/the_ai_botz"><img src="https://img.shields.io/badge/Telegram-Join%20Telegram%20Channel-yellow.svg?logo=telegram"></a>
---
+++ configs.py
@@ -0,0 +1,49 @@
+import os
+
+class Config(object):
+  API_ID = int(os.environ.get("API_ID", ""))
+  API_HASH = os.environ.get("API_HASH", "")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "tnshort.net")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "aa20b48ce6c0eb337b3e979b6b4aeef8a7b26447")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  ABOUT_BOT_TEXT = f"""
+This is a Permanent FileStore Bot. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+
+╭────[ 🔅Ai FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
+│
+├🔸 My Name: [Ai FileStore Bot](https://t.me/{BOT_USERNAME})
+│
+├🔸 Language: [Python 3](https://www.python.org)
+│
+├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
+│
+╰──────[ 😎 ]───────────⍟
+"""
+  ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [OWNER](https://t.me/Ai_botz_owner_bot)
+ 
+ AI DEVELOPERS PROJECT.
+
+[OPEN MENU](https://t.me/ai_botz_menu_bot)
+"""
+  HOME_TEXT = """
+Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
+
+How to Use Bot & it's Benefits??
+
+📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+
+⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+"""
